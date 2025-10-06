9f8ab304e6a3edefc6773453e0eba2458247e1df
Nickshoc <dimyshu@gmail.com>
2025-10-06 22:11:10 +0300

    Add files via upload

---
+++ sem2/lab1/demo/src/main/java/com/example/demo/MessageController.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MessageController {
+    private List<String> userMessages = new ArrayList<>();
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/messages")
+    public List<String> getAllMessages() {
+        return userMessages;
+    }
+
+    @PostMapping("/messages")
+    public String publishMessage(@RequestBody String message) {
+        userMessages.add(message);
+        return "Message published successfully!";
+    }
+
+    @PutMapping("/messages/{index}")
+    public String updateMessage(@PathVariable int index, @RequestBody String
+            message) {
+        if (index >= 0 && index < userMessages.size()) {
+            userMessages.set(index, message);
+            return "Message updated successfully!";
+        }
+        return "Message not found at index " + index;
+    }
+
+    @DeleteMapping("/messages/{index}")
+    public String deleteMessage(@PathVariable int index) {
+        if (index >= 0 && index < userMessages.size()) {
+            userMessages.remove(index);
+            return "Message deleted successfully!";
+        }
+        return "Message not found at index " + index;
+    }
+
+    @DeleteMapping("messages/clear")
+    public String clearAllMessages() {
+        int count = userMessages.size();
+        userMessages.clear();
+        return "Message cleared successfully! " + count + " messages";
+    }
+
+    @GetMapping("/messages/{index}")
+    public String getMessageByIndex(@PathVariable int index) {
+        if (index >= 0 && index < userMessages.size()) {
+            return userMessages.get(index);
+        }
+        return "Message not found at index " + index;
+    }
+}
+
+
+
